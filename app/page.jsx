76f2d1ff47a51e41ccb5c47278a79b7e5c2ac17b
anglczbla <angelicaizabella90@gmail.com>
2026-01-04 11:58:39 +0700

    pagination

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,25 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 import AnimeList from "./components/AnimeList";
 import Search from "./components/Search";
 
 export default function Home() {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
   const { isPending, error, data } = useQuery({
-    queryKey: ["anime"],
+    queryKey: ["anime", page, limit],
     queryFn: async () => {
-      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`;
+      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=${limit}&page=${page}`;
       console.log("Fetching URL:", url);
       const response = await axios.get(url);
       return response.data;
     },
   });
+
+  const totalPages = data?.pagination?.items.total;
+  console.log(totalPages);
 
   console.log("isi data", data);
 
@@ -23,6 +29,18 @@ export default function Home() {
   return (
     <div>
       <h1>FEATURED</h1>
+      {[...Array(totalPages)].map((_, idx) => (
+        <button
+          onClick={() => setPage((prev) => prev + 1)}
+          className={`w-10 h-10 flex items-center justify-center rounded-lg font-semibold transition ${
+            page === idx + 1
+              ? "bg-slate-800 text-white shadow-lg"
+              : "bg-white text-slate-600 border border-slate-200 hover:bg-slate-50"
+          }`}
+        >
+          {idx + 1}
+        </button>
+      ))}
       <Search />
       <AnimeList data={data} />
     </div>
